5b6785cb876286f0a345e4682499b89e2e376c8c
Brightnyandiko <carlsonbright98@gmail.com>
2025-05-07 17:31:53 +0300

    added few functionalities

---
+++ lib/database/habit_database.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:isar/isar.dart';
+
+class HabitDatabase extends ChangeNotifier {
+  static late Isar isar;
+
+  /*
+
+  S E T U P
+
+   */
+
+  // I N I T I A L I Z E - D A T A B A S E
+
+  //Save first date of app startup (for heatmap)
+
+  //Get first date of app startup (for heatmap)
+
+  /*
+
+  C R U D X O P E R A T I O N S
+
+   */
+
+  //List of habits
+
+  //C R E A T E -  add a new habit
+
+  //R E A D -saved habits from db
+
+  //U P D A T E  - Check habit and off habit
+
+ //U P D A T E  - Edit habit name
+}
